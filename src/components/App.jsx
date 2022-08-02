4d6e18191450ddf76aaabaa7120f4a5c64495a96
Mauro Reis <mauroacsreis@gmail.com>
2022-08-02 18:37:51 -0300

    Add V.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQHdFvilq2eA1w/profile-displayphoto-shrink_800_800/0/1649795706579?e=1664409600&v=beta&t=zoewkk9Vm_yrihdLZjDCLU885RYHrvvcIPe2Zm-lrJ8" />
+      {/* "Mapping" Technique used for contacts.js object array */}
+      {contacts.map(createCard)}
+      {/* Instead of */}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,6 +42,7 @@ function App() {
         tel={contacts[2].phone}
         email={contacts[2].email}
       />
+      */}
     </div>
   );
 }
